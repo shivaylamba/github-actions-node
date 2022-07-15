f72ad2655f0ad6e3b326b07bf128fdbe00266160
shivay <shivaylamba@gmail.com>
2022-07-16 05:01:52 +0530

    correct the error message

--- src/app.js
+++ src/app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/testNode', (_req, res) => {
-  res
-    .status(200)
-    .send('Yes the testNode endpoint worked and returned correct value');
+  res.status(200).send('Yes the testNode endpoint worked');
 });
 
 module.exports = app;
